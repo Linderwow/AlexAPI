f8469d08b184ee9793f101cf5bc60247047db4ea
Jamal <jamal@cybertekschool.com>
2022-04-19 16:24:59 -0400

    Day2: SpartanGetRequests.java test3 done

--- src/test/java/com/cydeo/day2/SpartanGetRequests.java
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SpartanGetRequests {
@@ -17,6 +18,7 @@ public class SpartanGetRequests {
         Then status code must be 200
         And response content type must be application/json
      */
+    @DisplayName("GET all spartans")
     @Test
     public void test1(){
 
@@ -47,6 +49,7 @@ public class SpartanGetRequests {
         And json body should contain 'Fidole'
      */
 
+    @DisplayName("GET individual spartan")
     @Test
     public void test2(){
 
@@ -64,4 +67,42 @@ public class SpartanGetRequests {
         Assertions.assertTrue(response.body().asString().contains("Fidole"));
     }
 
+     /*
+        Given no headers provided
+        When Users send GET request to /api/hello
+        Then response status code should be 200
+        And Content type header should be "text/plain;charset=UTF-8"
+        And header should contain Date
+        And Content-Length should be 17
+        And body should be “Hello from Sparta"
+        */
+    @DisplayName("GET request to /api/hello endpoint")
+    @Test
+    public void test3(){
+
+        Response response = RestAssured.when().get(url + "/api/hello");
+
+        response.prettyPrint();
+
+        //verify status code
+        Assertions.assertEquals(200,response.statusCode());
+
+        //verify content type
+        Assertions.assertEquals("text/plain;charset=UTF-8",response.contentType());
+
+        //verify Date header exists in Response headers
+        //we use hasHeaderWithName method to verify header exists or not - it returns boolean
+        Assertions.assertTrue(response.headers().hasHeaderWithName("Date"));
+
+        //to get header value we use header() method which accepts header name as parameter and return value as string
+        System.out.println("response.header(\"Content-Length\") = " + response.header("Content-Length"));
+        System.out.println("response.header(\"Connection\") = " + response.header("Connection"));
+
+        //verify content length is 17
+        Assertions.assertEquals("17",response.header("Content-Length"));
+
+        //verify body is "hello from sparta"
+        Assertions.assertEquals("Hello from Sparta",response.body().asString());
+
+    }
 }
