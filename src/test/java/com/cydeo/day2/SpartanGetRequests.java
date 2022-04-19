8cfa6eb2a5ee8e993b86b42ce1cc25730784708c
Jamal <jamal@cybertekschool.com>
2022-04-19 15:28:05 -0400

    Day2: SpartanGetRequests.java test1 done

--- src/test/java/com/cydeo/day2/SpartanGetRequests.java
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -1,5 +1,9 @@
 package com.cydeo.day2;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SpartanGetRequests {
@@ -15,6 +19,23 @@ public class SpartanGetRequests {
      */
     @Test
     public void test1(){
+
+        Response response = RestAssured.
+                given().
+                       accept(ContentType.JSON)
+                .when()
+                         .get(url + "/api/spartans");
+        //print the status code
+        System.out.println("response.statusCode() = " + response.statusCode());
+        //print the content type
+        System.out.println("response.contentType() = " + response.contentType());
+
+        //how to test API ?
+        //verify status code is 200
+        Assertions.assertEquals(200,response.statusCode());
+
+        //verify content type is application json
+        Assertions.assertEquals("application/json",response.contentType());
 
     }
 }
