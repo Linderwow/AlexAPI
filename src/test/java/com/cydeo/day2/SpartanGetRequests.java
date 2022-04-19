3f23e43f435a2a76c84c792d310b657f7d56dd72
Jamal <jamal@cybertekschool.com>
2022-04-19 14:58:20 -0400

    Day2: SpartanGetRequests.java added

---
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -0,0 +1,20 @@
+package com.cydeo.day2;
+
+import org.junit.jupiter.api.Test;
+
+public class SpartanGetRequests {
+
+
+    String url = "http://44.201.121.105:8000";
+
+    /*
+        Given Accept type is application/json
+        When user send GET request to  /api/spartans end point
+        Then status code must be 200
+        And response content type must be application/json
+     */
+    @Test
+    public void test1(){
+
+    }
+}
