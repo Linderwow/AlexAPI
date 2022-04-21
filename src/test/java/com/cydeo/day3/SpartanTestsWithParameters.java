baa54d4c2da2c9c8fd061e9643639d4ec61a8828
Jamal <jamal@cybertekschool.com>
2022-04-21 11:34:13 -0400

    Day3: SpartanTestsWithParameters.java test3 added

--- src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
+++ src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
@@ -67,4 +67,42 @@ public class SpartanTestsWithParameters extends SpartanTestBase {
 
     }
 
+    /*
+        Given Accept type is Json
+        And query parameter values are:
+        gender|Female
+        nameContains|e
+        When user sends GET request to /api/spartans/search
+        Then response status code should be 200
+        And response content-type: application/json
+        And "Female" should be in response payload
+        And "Janette" should be in response payload
+     */
+
+    @DisplayName("GET request to /api/spartans/search with Query Params")
+    @Test
+    public void test3(){
+
+       Response response = given().log().all()
+                .accept(ContentType.JSON)
+                .and().queryParam("gender","Female")
+                .and().queryParam("nameContains","e")
+        .when()
+                .get("/api/spartans/search");
+
+        //verify status code
+        assertEquals(200,response.statusCode());
+        //verify content type
+        assertEquals("application/json",response.contentType());
+        //verify Female inside body
+        assertTrue(response.body().asString().contains("Female"));
+        //verify Janette inside the json body
+        assertTrue(response.body().asString().contains("Janette"));
+
+
+
+
+
+
+    }
 }
