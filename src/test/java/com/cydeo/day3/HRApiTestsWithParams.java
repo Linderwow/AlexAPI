34b021c0e1a0de36ab35de961b003e0e0083a0e2
Jamal <jamal@cybertekschool.com>
2022-04-21 12:30:08 -0400

    Day3: HR task done

--- src/test/java/com/cydeo/day3/HRApiTestsWithParams.java
+++ src/test/java/com/cydeo/day3/HRApiTestsWithParams.java
@@ -1,7 +1,9 @@
 package com.cydeo.day3;
 
 import com.cydeo.utilities.HrTestBase;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
@@ -26,5 +28,28 @@ public class HRApiTestsWithParams extends HrTestBase {
         And Content type is application/json
         And Payload should contain "United States of America"
      */
+
+    @DisplayName("GET request to /countries with query param")
+    @Test
+    public void test2(){
+    Response response = given()
+                    .accept(ContentType.JSON)
+                    .and()
+                    .queryParam("q","{\"region_id\":2}")
+                    .log().all() //optional
+            .when()
+                    .get("/countries");
+
+      //verify status code
+        assertEquals(200,response.statusCode());
+       //verify content type
+        assertEquals("application/json",response.contentType());
+        // verify United States of America exists
+        assertTrue(response.body().asString().contains("United States of America"));
+
+
+
+    }
+
 
 }
