99e63f9bddbdcfeca42f056030733c4b9b05fcde
Jamal <jamal@cybertekschool.com>
2022-04-21 10:47:22 -0400

    Day4: SpartanTestsWithParameters.java test1 added

--- src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
+++ src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
@@ -1,6 +1,11 @@
 package com.cydeo.day3;
 
 import com.cydeo.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,9 +15,28 @@ public class SpartanTestsWithParameters extends SpartanTestBase {
           When user sends GET request to /api/spartans/{id}
           Then response status code should be 200
           And response content-type: application/json
-          And "Blythe" should be in response payload
+          And "Blythe" should be in response payload(body)
        */
 
+    @DisplayName("GET request to /api/spartans/{id} with ID 5")
+    @Test
+    public void test1(){
 
+   Response response =  given()
+                .accept(ContentType.JSON)
+                .and()
+                .pathParam("abc",5)
+        .when()
+                .get("/api/spartans/{abc}");
+
+   //verify status code
+   assertEquals(200,response.statusCode());
+   //verify content type
+    assertEquals("application/json",response.contentType());
+    //verify "Blythe" inside the payload
+    assertTrue(response.body().asString().contains("Blythe"));
+
+
+    }
 
 }
