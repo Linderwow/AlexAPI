8bac71d95ce35a229e36da915d34d52df47b72d5
Jamal <jamal@cybertekschool.com>
2022-04-21 15:29:54 -0400

    Day3: SpartanTestWithJsonPath added

---
+++ src/test/java/com/cydeo/day3/SpartanTestWithJsonPath.java
@@ -0,0 +1,7 @@
+package com.cydeo.day3;
+
+import com.cydeo.utilities.SpartanTestBase;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class SpartanTestWithJsonPath extends SpartanTestBase {
+}
