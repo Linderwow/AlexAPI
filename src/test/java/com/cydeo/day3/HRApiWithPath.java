e3432d0497905afa684bcb9fb9568a9ed2b52035
Jamal <jamal@cybertekschool.com>
2022-04-21 14:30:48 -0400

    Day3: HRApiWithPath.java added

---
+++ src/test/java/com/cydeo/day3/HRApiWithPath.java
@@ -0,0 +1,11 @@
+package com.cydeo.day3;
+
+import com.cydeo.utilities.HrTestBase;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HRApiWithPath extends HrTestBase {
+
+
+}
