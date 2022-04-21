73012cc6bb0d1b5c6945c5b127f5d2e712bb210b
Jamal <jamal@cybertekschool.com>
2022-04-21 12:18:56 -0400

    Day3: HR task added

--- src/test/java/com/cydeo/day3/HRApiTestsWithParams.java
+++ src/test/java/com/cydeo/day3/HRApiTestsWithParams.java
@@ -1,8 +1,30 @@
 package com.cydeo.day3;
 
 import com.cydeo.utilities.HrTestBase;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HRApiTestsWithParams extends HrTestBase {
+
+
+        @Test
+        public void test1(){
+            Response response = get("/regions");
+
+            response.prettyPrint();
+
+        }
+
+       /*
+        Given accept type is Json
+        And parameters: q = {"region_id":2}
+        When users sends a GET request to "/countries"
+        Then status code is 200
+        And Content type is application/json
+        And Payload should contain "United States of America"
+     */
+
 }
