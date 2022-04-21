5908b8d4f28ae532b8e53074dc55d06449b62165
Jamal <jamal@cybertekschool.com>
2022-04-21 10:36:08 -0400

    Day4: SpartanTestsWithParameters.java added

---
+++ src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
@@ -0,0 +1,18 @@
+package com.cydeo.day3;
+
+import com.cydeo.utilities.SpartanTestBase;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanTestsWithParameters extends SpartanTestBase {
+     /*   Given accept type is Json
+          And Id parameter value is 5
+          When user sends GET request to /api/spartans/{id}
+          Then response status code should be 200
+          And response content-type: application/json
+          And "Blythe" should be in response payload
+       */
+
+
+
+}
