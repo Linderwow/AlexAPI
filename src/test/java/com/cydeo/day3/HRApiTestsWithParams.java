06c02a37d4415053a4281819a681cfde7ffa4601
Jamal <jamal@cybertekschool.com>
2022-04-21 12:15:38 -0400

    Day3: HR added

---
+++ src/test/java/com/cydeo/day3/HRApiTestsWithParams.java
@@ -0,0 +1,8 @@
+package com.cydeo.day3;
+
+import com.cydeo.utilities.HrTestBase;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HRApiTestsWithParams extends HrTestBase {
+}
