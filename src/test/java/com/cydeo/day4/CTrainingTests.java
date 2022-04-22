d424d53a867161039594b00c51d627eb49b00319
Jamal <jamal@cybertekschool.com>
2022-04-22 11:57:19 -0400

    Day3: CTrainingTests.java task done

--- src/test/java/com/cydeo/day4/CTrainingTests.java
+++ src/test/java/com/cydeo/day4/CTrainingTests.java
@@ -1,6 +1,10 @@
 package com.cydeo.day4;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +33,55 @@ public class CTrainingTests {
 
                 using JsonPath
              */
+
+    @Test
+    public void test1(){
+
+     Response response= given().
+                        accept(ContentType.JSON)
+                        .and()
+                        .pathParam("abc",24103)
+                .when()
+                        .get("/student/{abc}");
+
+        response.prettyPrint();
+
+        //verify status code
+        assertEquals(200,response.statusCode());
+
+        //verify content type
+        assertEquals("application/json;charset=UTF-8",response.contentType());
+
+        //how to get any header value ?
+        assertEquals("gzip",response.header("Content-Encoding"));
+
+        //how to verify header exists or not ?
+        assertTrue(response.headers().hasHeaderWithName("Date"));
+
+
+        //payload/body verification
+        /*
+        firstName Karole                          --> students[0].firstName
+        batch 7                                   --> students[0].batch
+        major Master of Creative Arts             --> students[0].major
+        emailAddress hassan.lebsack@hotmail.com   --> students[0].contact.emailAddress
+        companyName Legacy Integration Analyst    --> students[0].company.companyName
+        street 6253 Willard Place                 --> students[0].company.address.street
+        zipCode 28524                             --> students[0].company.address.zipCode
+         */
+
+        //get jsonpath object
+        JsonPath jsonPath = response.jsonPath();
+
+        assertEquals("Karole",jsonPath.getString("students[0].firstName"));
+        assertEquals(7,jsonPath.getInt("students[0].batch"));
+        assertEquals("Master of Creative Arts",jsonPath.getString("students[0].major"));
+        assertEquals("hassan.lebsack@hotmail.com",jsonPath.getString("students[0].contact.emailAddress"));
+        assertEquals("Legacy Integration Analyst",jsonPath.getString("students[0].company.companyName"));
+        assertEquals("6253 Willard Place",jsonPath.getString("students[0].company.address.street"));
+        assertEquals(28524,jsonPath.getInt("students[0].company.address.zipCode"));
+
+    }
 
 
 
