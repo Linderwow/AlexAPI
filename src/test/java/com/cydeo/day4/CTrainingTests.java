bfb601e74930520bcd1bf75a3722874f341e200c
Jamal <jamal@cybertekschool.com>
2022-04-22 11:14:28 -0400

    Day3: CTrainingTests.java task ready

--- src/test/java/com/cydeo/day4/CTrainingTests.java
+++ src/test/java/com/cydeo/day4/CTrainingTests.java
@@ -11,4 +11,27 @@ public class CTrainingTests {
     public static void init(){
         RestAssured.baseURI ="http://api.cybertektraining.com";
     }
+
+    //send a get request to student id 24103 as a path parameter and accept header application/json
+    //verify status code=200
+    // /content type=application/json;charset=UTF-8
+    // /Content-Encoding = gzip
+    //verify Date header exists
+    //assert that
+            /*
+                firstName Karole
+                batch 7
+                major Master of Creative Arts
+                emailAddress hassan.lebsack@hotmail.com
+                companyName Legacy Integration Analyst
+                street 6253 Willard Place
+                zipCode 28524
+
+                using JsonPath
+             */
+
+
+
+
+
 }
