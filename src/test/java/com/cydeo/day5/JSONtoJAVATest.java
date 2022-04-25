eb4dc073cfce46214c2389f61986c83bbe24b70e
Jamal <jamal@cybertekschool.com>
2022-04-25 14:27:08 -0400

    Day5: JSONtoJAVATest.java added

---
+++ src/test/java/com/cydeo/day5/JSONtoJAVATest.java
@@ -0,0 +1,19 @@
+package com.cydeo.day5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+public class JSONtoJAVATest {
+
+    @DisplayName("GET one Spartan and deserialize to MAP")
+    @Test
+    public void test1(){
+
+
+
+    }
+
+}
