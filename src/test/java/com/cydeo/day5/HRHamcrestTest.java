17f217718b119c3344b693c59b01acfecfe0f126
Jamal <jamal@cybertekschool.com>
2022-04-25 12:53:40 -0400

    Day5: HRHamcrestTest.java done

--- src/test/java/com/cydeo/day5/HRHamcrestTest.java
+++ src/test/java/com/cydeo/day5/HRHamcrestTest.java
@@ -1,6 +1,9 @@
 package com.cydeo.day5;
 
 import com.cydeo.utilities.HrTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,7 +29,42 @@ public class HRHamcrestTest extends HrTestBase {
         //expected names
         List<String> names = Arrays.asList("Alexander","Bruce","David","Valli","Diana");
 
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParam("q","{\"job_id\":\"IT_PROG\"}")
+        .when()
+                .get("/employees")
+        .then()
+                .statusCode(200)
+                .and()
+                .contentType("application/json")
+                .body("items.job_id",everyItem(equalTo("IT_PROG")))
+                .body("items.salary",everyItem(greaterThan(3000)))
+                .body("items.first_name",equalTo(names))
+                .body("items.email",containsInAnyOrder("DAUSTIN","AHUNOLD","BERNST","VPATABAL","DLORENTZ"));
 
+    }
+
+
+    @Test
+    public void test2(){
+        //we want to chain with hamcrest and also get the response object.
+        JsonPath jsonPath = given()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParam("q","{\"job_id\":\"IT_PROG\"}")
+                .when()
+                .get("/employees")
+        .then()
+                .statusCode(200)
+                 .body("items.job_id",everyItem(equalTo("IT_PROG")))
+                .extract().response().jsonPath();
+
+        //extract() --> method that allow us to get response object after we use then() method.
+
+        //assert that we have only 5 firstnames
+        assertThat(jsonPath.getList("items.firstname"),hasSize(5));
 
 
 
