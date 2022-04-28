cac139e6de4859161771916bb5eba0f77a5c6b97
Jamal <jamal@cybertekschool.com>
2022-04-28 12:30:08 -0400

    Day7: SpartanPostRequestDemo.java test2()

--- src/test/java/com/cydeo/day7/SpartanPostRequestDemo.java
+++ src/test/java/com/cydeo/day7/SpartanPostRequestDemo.java
@@ -6,6 +6,9 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
@@ -52,6 +55,37 @@ public class SpartanPostRequestDemo extends SpartanTestBase {
 
         assertThat(response.path("success"),is(expectedMessage));
         assertThat(response.path("data.name"),is("Severus"));
+        assertThat(response.path("data.gender"),is("Male"));
+        assertThat(response.path("data.phone"),is(8877445596l));
+
+        response.prettyPrint();
+    }
+
+    @DisplayName("POST a spartan with MAP")
+    @Test
+    public void test2(){
+
+        Map<String,Object> requestMap = new LinkedHashMap<>();
+        requestMap.put("gender","Male");
+        requestMap.put("name","Severus Snape");
+        requestMap.put("phone",8877445596l);
+
+
+        Response response = given().accept(ContentType.JSON).log().all() // what we are asking from api which is JSON response
+                .and()
+                .contentType(ContentType.JSON) //what we are sending to api, which is JSON request body
+                .body(requestMap)
+                .when()
+                .post("/api/spartans");
+
+        //verify status code
+        assertThat(response.statusCode(),is(201));
+        assertThat(response.contentType(),is("application/json"));
+
+        String expectedMessage = "A Spartan is Born!";
+
+        assertThat(response.path("success"),is(expectedMessage));
+        assertThat(response.path("data.name"),is("Severus Snape"));
         assertThat(response.path("data.gender"),is("Male"));
         assertThat(response.path("data.phone"),is(8877445596l));
 
