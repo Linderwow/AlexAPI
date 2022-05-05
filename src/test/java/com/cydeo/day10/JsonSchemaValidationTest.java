1a0d32a547a060cecb3f160405877a99860bbb0d
Jamal <jamal@cybertekschool.com>
2022-05-05 14:41:53 -0400

    Day10: SingleSpartanSchema.json test1 added

---
+++ src/test/java/com/cydeo/day10/JsonSchemaValidationTest.java
@@ -0,0 +1,23 @@
+package com.cydeo.day10;
+
+import com.cydeo.utilities.SpartanAuthTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class JsonSchemaValidationTest extends SpartanAuthTestBase {
+
+
+    @DisplayName("GET request to verify one spartan against to schema")
+    @Test
+    public void test1(){
+
+
+
+    }
+
+
+}
