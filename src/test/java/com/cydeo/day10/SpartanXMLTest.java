0854a90c00cf5aba50ec8de89e67356be4c5503d
Jamal <jamal@cybertekschool.com>
2022-05-05 10:40:19 -0400

    Day10: SpartanXMLTest.java test1 is done

--- src/test/java/com/cydeo/day10/SpartanXMLTest.java
+++ src/test/java/com/cydeo/day10/SpartanXMLTest.java
@@ -1,9 +1,39 @@
 package com.cydeo.day10;
 
 
+import com.cydeo.utilities.SpartanAuthTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 
-public class SpartanXMLTest {
+public class SpartanXMLTest extends SpartanAuthTestBase {
+
+    //we will ask for xml result
+    //assert status code
+    //assert content type xml
+    //verify first name
+
+    @Test
+    public void test1(){
+
+            given()
+                    .accept(ContentType.XML)
+                    .and()
+                    .auth().basic("admin","admin")
+            .when()
+                    .get("/api/spartans")
+            .then()
+                    .statusCode(200)
+                    .and()
+                    .contentType("application/xml")
+                    .body("List.item[0].name",is("Rodolfo"))
+                    .body("List.item[0].gender",is("Male"))
+                    .log().all();
+
+    }
+
+
 }
