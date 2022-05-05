97e8e447cc35b39f9e0ea4d956dcae423f2995ea
Jamal <jamal@cybertekschool.com>
2022-05-05 10:26:56 -0400

    Day10: SpartanXMLTest.java added

---
+++ src/test/java/com/cydeo/day10/SpartanXMLTest.java
@@ -0,0 +1,9 @@
+package com.cydeo.day10;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class SpartanXMLTest {
+}
