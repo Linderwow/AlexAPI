0a02d3d2e60b9b8f5e3566658a5c1ffdc2447402
Jamal <jamal@cybertekschool.com>
2022-05-05 12:49:43 -0400

    Day10: ResponseTimeTest.java completed

--- src/test/java/com/cydeo/day10/ResponseTimeTest.java
+++ src/test/java/com/cydeo/day10/ResponseTimeTest.java
@@ -1,6 +1,8 @@
 package com.cydeo.day10;
 
 import com.cydeo.utilities.SpartanAuthTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,20 @@ public class ResponseTimeTest extends SpartanAuthTestBase {
     @DisplayName("GET request to all spartans and verify response time")
     @Test
     public void test1(){
+
+        Response response = given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin", "admin")
+                .when().get("/api/spartans")
+                .then()
+                .statusCode(200)
+                .and()
+                .time(both(greaterThan(500L)).and(lessThan(1200L)))
+                .extract().response();
+
+        System.out.println("response.getTime() = " + response.getTime());
+
 
     }
 }
