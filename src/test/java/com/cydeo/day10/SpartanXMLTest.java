9d332f0d3b979d8996d01c4373186d43bb3fbcfa
Jamal <jamal@cybertekschool.com>
2022-05-05 10:47:56 -0400

    Day10: SpartanXMLTest.java test2

--- src/test/java/com/cydeo/day10/SpartanXMLTest.java
+++ src/test/java/com/cydeo/day10/SpartanXMLTest.java
@@ -3,6 +3,10 @@ package com.cydeo.day10;
 
 import com.cydeo.utilities.SpartanAuthTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -32,6 +36,30 @@ public class SpartanXMLTest extends SpartanAuthTestBase {
                     .body("List.item[0].name",is("Rodolfo"))
                     .body("List.item[0].gender",is("Male"))
                     .log().all();
+
+    }
+
+    @DisplayName("GET all spartan with XML")
+    @Test
+    public void test2(){
+
+        Response response = given().accept(ContentType.XML)
+                .and()
+                .auth().basic("admin", "admin")
+                .when().get("/api/spartans");
+
+
+        //get response xml body/payload and save inside the xmlpath object
+        XmlPath xmlPath = response.xmlPath();
+
+        //get first spartan name
+        String name = xmlPath.getString("List.item[0].name");
+        System.out.println("name = " + name);
+
+        //get me 3rd spartan id number
+        int id = xmlPath.getInt("List.item[2].id");
+        System.out.println("id = " + id);
+
 
     }
 
