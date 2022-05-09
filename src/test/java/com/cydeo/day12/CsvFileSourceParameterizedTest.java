a7881f1858906e99878a4fe01b8d0526fd727f84
Jamal <jamal@cybertekschool.com>
2022-05-09 10:38:04 -0400

    Day12: CsvFileSourceParameterizedTest.java added

--- src/test/java/com/cydeo/day12/CsvFileSourceParameterizedTest.java
+++ src/test/java/com/cydeo/day12/CsvFileSourceParameterizedTest.java
@@ -1,10 +1,24 @@
 package com.cydeo.day12;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static io.restassured.RestAssured.*;
 
 public class CsvFileSourceParameterizedTest {
+
+    // Write a parameterized test for this request
+    // Get the data from csv source
+    // GET https://api.zippopotam.us/us/{state}/{city}
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/zipcode.csv",numLinesToSkip = 1)
+    public void zipCodeTestWithFile(String state,String city,int zipCount){
+        System.out.println("state = " + state);
+        System.out.println("city = " + city);
+        System.out.println("zipCount = " + zipCount);
+
+    }
 
 
 }
