f57567f99f985049168147da2281ced2ad4fe63a
Jamal <jamal@cybertekschool.com>
2022-05-09 11:39:35 -0400

    Day12: ExcelUtil.java added

--- src/test/java/com/cydeo/day12/MethodSourceParameterizedTest.java
+++ src/test/java/com/cydeo/day12/MethodSourceParameterizedTest.java
@@ -1,10 +1,12 @@
 package com.cydeo.day12;
 
+import com.cydeo.utilities.ExcelUtil;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,6 +24,24 @@ public class MethodSourceParameterizedTest {
 
     }
 
+    @ParameterizedTest
+    @MethodSource("getExcelData")
+    public void excelParamTest(Map<String,String> userInfo){
+        System.out.println("userInfo.get(\"firstname\") = " + userInfo.get("firstname"));
+        System.out.println("userInfo.get(\"lastname\") = " + userInfo.get("lastname"));
+
+    }
+
+    public static List<Map<String,String>> getExcelData(){
+        ExcelUtil vytrackFile = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx","QA3-all");
+
+        List<Map<String, String>> dataList = vytrackFile.getDataList();
+
+        return  dataList;
+    }
+
+
+
 
     public static List<String> getNames(){
         //you can get value from anywhere almost anytype and return to your test
@@ -32,5 +52,6 @@ public class MethodSourceParameterizedTest {
         List<String> nameList = Arrays.asList("Muhtar","Asya","Gurhan","Adam","Akbar","Aysun","Zulfikar");
         return nameList;
     }
+
 
 }
