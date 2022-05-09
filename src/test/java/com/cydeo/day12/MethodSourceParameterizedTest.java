2a981e4b21f5776b0891fe1acc4ef774522fa91e
Jamal <jamal@cybertekschool.com>
2022-05-09 10:48:54 -0400

    Day12: MethodSourceParameterizedTest added

---
+++ src/test/java/com/cydeo/day12/MethodSourceParameterizedTest.java
@@ -0,0 +1,10 @@
+package com.cydeo.day12;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class MethodSourceParameterizedTest {
+}
