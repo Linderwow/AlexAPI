9ed0bc2d3ce015729a4d786de70a33e05701ed78
Jamal <jamal@cybertekschool.com>
2022-05-09 16:22:13 -0400

    @SerenityTest and serenity.properties files are added

--- src/test/java/b25/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/b25/spartan/admin/SpartanAdminGetTest.java
@@ -2,12 +2,16 @@ package b25.spartan.admin;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
+
+
+@SerenityTest
 public class SpartanAdminGetTest {
 
     //beforeAll is the same thing with beforeClass in testng
