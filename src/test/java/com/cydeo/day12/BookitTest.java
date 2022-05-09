556fd4062f6d631d78e2dfbf4e63679cb52a0798
Jamal <jamal@cybertekschool.com>
2022-05-09 11:43:50 -0400

    Day12: BookItQa3.xlsx added

---
+++ src/test/java/com/cydeo/day12/BookitTest.java
@@ -0,0 +1,11 @@
+package com.cydeo.day12;
+
+public class BookitTest {
+
+    //create a method to read bookitqa3 excel file information
+    //use those information as an email and password to send a get request to /sign endpoint
+    //verify you got 200 for each request
+    //print accesstoken for each request
+    //https://cybertek-reservation-api-qa3.herokuapp.com
+
+}
