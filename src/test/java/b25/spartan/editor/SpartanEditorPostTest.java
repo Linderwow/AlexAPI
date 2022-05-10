69cd010afed815d0b561e50114e5f67a74bd5215
Jamal <jamal@cybertekschool.com>
2022-05-10 12:25:16 -0400

    SpartanEditorPostTest.java test1 done

--- src/test/java/b25/spartan/editor/SpartanEditorPostTest.java
+++ src/test/java/b25/spartan/editor/SpartanEditorPostTest.java
@@ -54,8 +54,33 @@ public class SpartanEditorPostTest extends SpartanNewBase {
          */
 
 
+        //status code is 201
+        Ensure.that("Status code is 201", x -> x.statusCode(201));
 
+        //content type is Json
+        Ensure.that("Content-type is JSON", vR -> vR.contentType(ContentType.JSON));
 
+        //A Spartan is Born!
+        Ensure.that("success message is correct", vR -> vR.body("success",is("A Spartan is Born!")));
+
+        //id is not null
+        Ensure.that("ID is not null",vR -> vR.body("data.id",notNullValue()));
+
+        //name is correct
+        Ensure.that("name is correct",vR -> vR.body("data.name",is(randomSpartanMap.get("name"))));
+
+        //gender is correct
+        Ensure.that("gender is correct",vR -> vR.body("data.gender",is(randomSpartanMap.get("gender"))));
+
+        //phone is correct
+        Ensure.that("phone is correct",vR -> vR.body("data.phone",is(randomSpartanMap.get("phone"))));
+
+        //check location header ends with newly generated id
+        String id = lastResponse().jsonPath().getString("data.id");
+
+        Ensure.that("check location header ends with newly generated id",
+                    vR -> vR.header("Location",endsWith(id))
+                );
 
     }
 
