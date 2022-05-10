b89bc9f72a43ff23a260522940f053eb16a1abf8
Jamal <jamal@cybertekschool.com>
2022-05-10 11:31:06 -0400

    SpartanEditorPostTest.java added

---
+++ src/test/java/b25/spartan/editor/SpartanEditorPostTest.java
@@ -0,0 +1,18 @@
+package b25.spartan.editor;
+
+import utilities.SpartanNewBase;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.Ensure;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static net.serenitybdd.rest.SerenityRest.given;
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class SpartanEditorPostTest extends SpartanNewBase {
+
+}
